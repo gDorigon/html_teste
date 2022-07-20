9bbdffc176df333f74394e689814995bea08eef7
Guilherme <guidbovo@gmail.com>
2022-07-20 17:21:45 -0300

    Todo list updates 20/07

--- todoLabs/main.js
+++ todoLabs/main.js
@@ -1,65 +1,71 @@
-async function apiCreateTask (description, complete){
+async function apiCreateTask(description, complete) {
 
-    const newTask = {description, complete}
-
+    const newTask = { description, complete }
 
     const url = 'http://localhost:3000/tasks';
     const options = {
         method: "POST",
-        headers:{
+        headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(newTask)
     }
     const response = await fetch(url, options);
 
-
     return await response.json();
 }
 
-async function apiGetTasks(){
+async function apiGetTasks() {
     const url = 'http://localhost:3000/tasks';
     const response = await fetch(url);
     return await response.json();
 }
 
-async function apiUpdateTask(id, task){
+async function apiUpdateTask(id, task) {
 
     const url = `http://localhost:3000/tasks/${id}`;
-    fetch (url, {
-       method: "PUT",
-        headers:{
+    fetch(url, {
+        method: "PUT",
+        headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(task)
     })
+}
 
-
-} 
-
-
-
-async function apiDeleteTask(id){
+async function apiDeleteTask(id) {
 
     const url = `http://localhost:3000/tasks/${id}`
-    fetch (url, {
-       method: "DELETE",
-        headers:{
+    fetch(url, {
+        method: "DELETE",
+        headers: {
             'Content-Type': 'application/json'
         },
-
     })
-
 }
- 
-// apiCreateTask("DAVIZAO", true);
 
-apiGetTasks();
+// apiUpdateTask(116, {
+//     description: "Dorigas",
+//     complete: true,
+// })
 
-apiUpdateTask(116, {
-    description: "Dorigas",
-    complete: true,
 
-})
+const addElement = querySelector("#add");
+addElement.onclick = async => {
+    
+    console.log("teste")
+    const tarefa = document.createElement("div");
+    tarefa.classList.add("itens");
+    
+    tarefa.innerText = "teste"
+    
+    document.getElementById("lista").appendChild(tarefa);
+}
 
-// apiDeleteTask(62)
+
+
+// var cb = document.querySelector(".checkbox-tarefa");
+// cb.setAttribute("checked", "checked");
+
+
+
